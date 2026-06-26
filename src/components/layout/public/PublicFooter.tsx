157940133034f59346095d9c46a2e27f8d27142a
Mohamed Sayed <midoseka8@gmail.com>
2026-06-27 01:39:15 +0300

    remove berry flavor item from flavor data

--- src/components/layout/public/PublicFooter.tsx
+++ src/components/layout/public/PublicFooter.tsx
@@ -68,6 +68,7 @@ export function PublicFooter() {
         src="/assets/hero/dark-roast.png"
         alt=""
         fill
+        priority
         sizes="100vw"
         className="object-cover opacity-[0.06]"
         aria-hidden="true"
